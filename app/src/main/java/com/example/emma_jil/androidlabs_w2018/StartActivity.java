c7ff4166b93bfbf672d0ae2a65cdff42886067ea
emma_jil <jile0007@algonquinlive.com>
2018-02-03 17:46:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/emma_jil/androidlabs_w2018/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.emma_jil.androidlabs_w2018;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
