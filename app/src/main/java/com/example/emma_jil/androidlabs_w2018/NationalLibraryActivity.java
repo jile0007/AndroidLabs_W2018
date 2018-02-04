c42db94a7c294150bb8e659b7ebef3742edfdf11
emma_jil <jile0007@algonquinlive.com>
2018-02-03 20:47:48 -0500

    Lab02_finish

--- app/src/main/java/com/example/emma_jil/androidlabs_w2018/NationalLibraryActivity.java
+++ app/src/main/java/com/example/emma_jil/androidlabs_w2018/NationalLibraryActivity.java
@@ -1,13 +1,50 @@
 package com.example.emma_jil.androidlabs_w2018;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NationalLibraryActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "NationalLibraryActivity";
+    Button loginB;
+    Button registerB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_national_library);
+
+        loginB = (Button)findViewById(R.id.LoginButton);
+        loginB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(NationalLibraryActivity.this, StartActivity.class);
+
+                Context c = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(c, R.string.LibraryLoginResult, duration);
+                toast.show();
+
+                startActivity(i);
+            }
+        });
+
+        registerB = (Button)findViewById(R.id.RegisterButton);
+        registerB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(NationalLibraryActivity.this, StartActivity.class);
+
+                Context c = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(c, R.string.LibraryRegisterResult, duration);
+                toast.show();
+
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/emma_jil/androidlabs_w2018/StartActivity.java
+++ app/src/main/java/com/example/emma_jil/androidlabs_w2018/StartActivity.java
@@ -1,15 +1,29 @@
 package com.example.emma_jil.androidlabs_w2018;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
 
-
+    protected static final String ACTIVITY_NAME = "StartActivity";
+    Button startGoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(ACTIVITY_NAME, "In onStart");
         setContentView(R.layout.activity_start);
+
+        startGoButton = (Button)findViewById(R.id.startGo);
+        startGoButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(StartActivity.this, NationalLibraryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
